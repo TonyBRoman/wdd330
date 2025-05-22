2946f9f8a69564db46033b104f5aab375b3adf9f
TonyBRoman <tonybaggon@gmail.com>
2025-05-21 20:20:24 -0600

    Dynamic Product List

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,13 @@ export function getParam(param) {
   const product = urlParams.get(param);
   return product
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if(clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlString = list.map(templateFn).jpoin("");
+  parentElement.insertAdjacentHTML(position, htmlStrings);
+  
+}
